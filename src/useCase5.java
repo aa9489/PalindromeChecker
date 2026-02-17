6e543fe3e1b5b3bfda4a03f22e59b4fbc774a3fc
aa9489 <aa9489.srmist.edu.in>
2026-02-17 11:19:44 +0530

    5th one added

---
+++ src/useCase5.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+import java.util.Stack;
+public class useCase {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        Stack<Character> stack = new Stack<>();
+        System.out.print("Enter a string: ");
+        String input = sc.nextLine();
+
+        for (int i = 0; i < input.length(); i++) {
+            stack.push(input.charAt(i));
+        }
+
+        String reversed = "";
+        while (!stack.isEmpty()) {
+            reversed = reversed + stack.pop();
+        }
+
+        if (input.equals(reversed)) {
+            System.out.println("The given string is a palindrome.");
+        } else {
+            System.out.println("The given string is not a palindrome.");
+        }
+
+        sc.close();
+    }
+}
