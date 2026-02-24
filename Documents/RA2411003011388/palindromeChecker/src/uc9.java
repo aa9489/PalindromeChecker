0dcaeb8bbfb854713d505031f4ecd2efac5a9bd9
aa9489 <aa9489@srmist.eddu.in>
2026-02-24 09:40:13 +0530

    uc9 completed

---
+++ Documents/RA2411003011388/palindromeChecker/src/uc9.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class uc9 {
+    public static boolean isPalindrome(String str, int start, int end) {
+        if (start >= end) return true;
+        if (str.charAt(start) != str.charAt(end)) return false;
+        return isPalindrome(str, start + 1, end - 1);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine().toLowerCase().replaceAll("[^a-z0-9]", "");
+        if (isPalindrome(input, 0, input.length() - 1)) {
+            System.out.println("Palindrome");
+        } else {
+            System.out.println("Not Palindrome");
+        }
+        sc.close();
+    }
+}
